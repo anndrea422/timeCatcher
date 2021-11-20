0c910f0ad6416ff3037a2ad061429714b4af12e3
andreataht <andreataht@gmail.com>
2021-11-20 14:09:25 +0200

    Initial commit

---
+++ src/main/java/com/example/timeCatcher/TimeCatcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.timeCatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeCatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeCatcherApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/timeCatcher/User.java
@@ -0,0 +1,21 @@
+package com.example.timeCatcher;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+
+
+public class User {
+    @org.springframework.data.annotation.Id // @Id ?
+    Integer Id;
+    String name;
+}
